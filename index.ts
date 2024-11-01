9fe560abb9708d759142af9d8bbd1a2e8c4cc351
Mohit <mohit.peak@gmail.com>
2024-11-01 15:11:59 +0530

    Added frontend's dist

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Application } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
 
 import CONFIG from "./src/config/server_config";
 import apiRouter from "./src/routes";
@@ -13,8 +14,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
+// Serve static files from the "dist" directory
+app.use(express.static(path.join(__dirname, "dist")));
+
 // API routes
 app.use("/api", apiRouter);
+
+// Fallback route to serve React app's index.html for any unknown routes
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
